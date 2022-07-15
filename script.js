d5b19b6751d8acd83f2d6a8c0724d025c961522a
jon-shogren <jon.shogren@mailchimp.com>
2022-07-15 10:26:24 -0400

    prompts mostly setup, logs answers. initial arrays setup

--- script.js
+++ script.js
@@ -1,17 +1,87 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var useUpperCase = "";
+var useLowerCase = "";
+var useSpecial = "";
+var useNumber = "";
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", () => {    
+  askUpper();
+  askLower();
+  askSpecial()
+  askNumber();
+  writePassword();
+  results();
+});
 
-var passwordLength = document.getElementById('charactercount');
-console.log(passwordLength);
 
-var uppercase = document.getElementById('uppercase');
-var lowercase = document.getElementById('lowercase');
-var numbers = document.getElementById('numbers');
-var special = document.getElementById('special');
+// ask if the user wants upper case characters in the password
+function askUpper() {
+  let upperCaseChoice = prompt("Do you want to require Upper Case characters?");
+  if (upperCaseChoice.toLowerCase() == "yes") {
+    useUpperCase = true;
+    alert("Ok, upper case characters will be used.");
+  } else if (upperCaseChoice.toLowerCase() == "no") {
+    useUpperCase = false;
+    alert("Ok, no upper case characters will be used.");
+  } else {
+    alert("Please enter 'Yes' or 'No'");
+    askUpper();
+  }
+  console.log(useUpperCase);
+}
 
+function askLower() {
+  let lowerCaseChoice = prompt("Do you want to require Lower Case characters?");
+  if (lowerCaseChoice.toLowerCase() == "yes") {
+    useLowerCase = true;
+    alert("Ok, lower case characters will be used.");
+  } else if (lowerCaseChoice.toLowerCase() == "no") {
+    useLowerCase = false;
+    alert("Ok, no lower case characters will be used.");
+  } else {
+    alert("Please enter 'Yes' or 'No'");
+    askLower();
+  }
+  console.log(useLowerCase);
+}
+
+
+function askSpecial() {
+  let specialChoice = prompt("Do you want to require Special characters?");
+  if (specialChoice.toLowerCase() == "yes") {
+    useSpecial = true;
+    alert("Ok, special characters will be used.");
+  } else if (specialChoice.toLowerCase() == "no") {
+    useSpecial = false;
+    alert("Ok, no special characters will be used.");
+  } else {
+    alert("Please enter 'Yes' or 'No'");
+    askSpecial();
+  }
+  console.log(useSpecial);
+}
+
+function askNumber() {
+  let numberChoice = prompt("Do you want to require Numbers?");
+  if (numberChoice.toLowerCase() == "yes") {
+    useNumber = true;
+    alert("Ok, numbers will be used.");
+  } else if (numberChoice.toLowerCase() == "no") {
+    useNumber = false;
+    alert("Ok, no numbers will be used.");
+  } else {
+    alert("Please enter 'Yes' or 'No'");
+    askNumber();
+  }
+  console.log(useNumber);
+}
+
+function results() {
+  console.log("Results:", useUpperCase, useLowerCase, useNumber, useSpecial);
+}
 
 // function that generates the password
 function generatePassword() {
@@ -25,6 +95,17 @@ function writePassword() {
 
   passwordText.value = password;
 
+  //just for visualizing
+  passwordText.value = "password!";
+
+
 }
 
 
+specialCharactersArray = ["!","\"","#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+numbersArray = [0,1,2,3,4,5,6,7,8,9];
+
+uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
