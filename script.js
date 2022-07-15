096271c773147150c563b88e8ff8894c4abeac81
jon-shogren <jon.shogren@mailchimp.com>
2022-07-14 20:41:22 -0400

    modifiers added, initial variables set

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+var passwordLength = document.getElementById('charactercount');
+console.log(passwordLength);
+
+var uppercase = document.getElementById('uppercase');
+var lowercase = document.getElementById('lowercase');
+var numbers = document.getElementById('numbers');
+var special = document.getElementById('special');
+
+
+// function that generates the password
+function generatePassword() {
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,5 +27,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
